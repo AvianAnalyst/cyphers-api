4fe40bd2bbf307d55c172aaa4711c91e04297d97
Kylan <aviananalyst@gmail.com>
2019-10-11 10:27:09 -0700

    Updated settings for deployment

--- cypher_deck/settings.py
+++ cypher_deck/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["181.206.60.71"]
 
 
 # Application definition
@@ -124,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
